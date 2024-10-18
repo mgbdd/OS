9d02255fdef20c6462d71fb0bac5471862a58a0f
mgbdd <132687128+mgbdd@users.noreply.github.com>
2024-10-18 22:05:57 +0700

    Create 1_5.c

---
+++ II_1/1_5.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+void sigint_handler(int signo) {
+    printf("Received SIGINT in thread %ld. Exiting\n", pthread_self());
+    exit(0);
+
+}
+
+void* block_signals_thread(void* arg) {
+    sigset_t mask;
+    sigfillset(&mask);
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);
+
+    while (1) {
+        printf("Signal blocking thread %ld is running\n", pthread_self());
+        sleep(2);
+    }
+    return NULL;
+}
+
+void* sigint_handler_thread(void* arg) {
+    struct sigaction sa;
+    sa.sa_handler = sigint_handler; 
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    while (1) {
+        printf("SIGINT handler thread %ld is running\n", pthread_self());
+        sleep(2); 
+    }
+    return NULL;
+}
+
+void* sigquit_handler_thread(void* arg) {
+    sigset_t mask;
+    int sig;
+    sigemptyset(&mask); 
+    sigaddset(&mask, SIGQUIT);
+
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);
+
+    while (1) {
+        printf("Waiting for SIGQUIT in thread %ld\n", pthread_self());
+        sigwait(&mask, &sig);
+        printf("Received SIGQUIT in thread %ld. Exiting\n", pthread_self());
+        break; 
+    }
+    return NULL;
+}
+
+int is_blocked(int signo) {
+    sigset_t current_mask;
+
+    if (sigprocmask(SIG_BLOCK, NULL, &current_mask) == -1) {
+        perror("sigprocmask");
+        exit(EXIT_FAILURE);
+    }
+
+    return sigismember(&current_mask, signo);
+}
+
+void block_quit()
+{
+    sigset_t mask;
+    sigemptyset(&mask); 
+    sigaddset(&mask, SIGQUIT);
+
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);
+    printf("\nSIGQUIT was blocked in main thread\n\n");
+
+}
+
+int main() {
+
+    if (is_blocked(SIGQUIT)) {
+        printf("Signal SIGQUIT is blocked in the main thread.\n");
+    } else {
+        printf("Signal SIGQUIT is NOT blocked in the main thread.\n");
+        block_quit();
+    }
+
+    pthread_t tid1, tid2, tid3;
+
+    pthread_create(&tid1, NULL, block_signals_thread, NULL);
+    
+    pthread_create(&tid2, NULL, sigint_handler_thread, NULL);
+
+    pthread_create(&tid3, NULL, sigquit_handler_thread, NULL);
+
+    // Соединяем потоки
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+
+    return 0;
+}
