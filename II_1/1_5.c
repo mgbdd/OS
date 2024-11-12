f5afb28186c99f898649d81600bb7d9090dae1a4
mgbdd <132687128+mgbdd@users.noreply.github.com>
2024-11-13 01:52:02 +0700

    Add files via upload

--- II_1/1_5.c
+++ II_1/1_5.c
@@ -7,7 +7,7 @@
 
 void sigint_handler(int signo) {
     printf("Received SIGINT in thread %ld. Exiting\n", pthread_self());
-    exit(0);
+    pthread_exit(0);
 
 }
 
@@ -69,38 +69,49 @@ int is_blocked(int signo) {
     return sigismember(&current_mask, signo);
 }
 
-void block_quit()
+void block_signal(int signo)
 {
     sigset_t mask;
     sigemptyset(&mask); 
-    sigaddset(&mask, SIGQUIT);
+    sigaddset(&mask, signo);
 
     pthread_sigmask(SIG_BLOCK, &mask, NULL);
-    printf("\nSIGQUIT was blocked in main thread\n\n");
+    printf("\nSIGNAL № %d was blocked in main thread\n\n", signo);
 
 }
 
 int main() {
 
+    printf("Main thread: %ld\n", pthread_self());
+
     if (is_blocked(SIGQUIT)) {
         printf("Signal SIGQUIT is blocked in the main thread.\n");
     } else {
         printf("Signal SIGQUIT is NOT blocked in the main thread.\n");
-        block_quit();
+        block_signal(SIGQUIT);
     }
+
+    /*if (is_blocked(SIGINT)) {
+        printf("Signal SIGINT is blocked in the main thread.\n");
+    }  else {
+        printf("Signal SIGINT is NOT blocked in the main thread.\n");
+        block_signal(SIGINT);
+    }*/
+
 
     pthread_t tid1, tid2, tid3;
 
     pthread_create(&tid1, NULL, block_signals_thread, NULL);
-    
     pthread_create(&tid2, NULL, sigint_handler_thread, NULL);
-
     pthread_create(&tid3, NULL, sigquit_handler_thread, NULL);
 
-    // Соединяем потоки
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
     pthread_join(tid3, NULL);
 
     return 0;
 }
+
+
+//SIGINT что-то странное, он должен видимо останавливать только 1 поток, а останавливает главный тоже
+
